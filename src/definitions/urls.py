e9ee74735e988efce5806e53c2358ad552156b45
Jay Alfaras <alfarasjb@gmail.com>
2024-07-03 12:21:17 +0800

    fix url

--- src/definitions/urls.py
+++ src/definitions/urls.py
@@ -4,7 +4,7 @@ class Urls:
     def __init__(self):
         self.server_host = 'expense-tracker-server.fly.dev'
         self.server_port = 3000
-        self.base_url = f'http://{self.server_host}:{self.server_port}'
+        self.base_url = f'http://{self.server_host}'
         self.db_url = f'{self.base_url}/api/db'
 
     def monthly_data_endpoint(self) -> str:
